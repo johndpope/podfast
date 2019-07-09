458bafe8127979361300f6ed86ff4bb4da3a434a
Orestis Papadopoulos <orestis@accusonus.com>
2019-07-09 23:51:18 +0300

    Xcode Project Setup

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodFast
+//
+//  Created by Orestis on 09/07/2019.
+//  Copyright © 2019 Orestis Papadopoulos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
